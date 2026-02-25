dcb3f6e7c183487032c33f2679973198fa1eb85e
ybhavya330-lgtm <ybhavya330@gmail.com>
2026-02-25 11:42:55 +0530

    Add datetime display and button click message

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("datetime").innerHTML =document.getElement().toLocaleString();
+
+function changeMessage() {
+    document.getElementById("message").innerHTML =
+        "You clicked the button! 🚀";
+}
